c128e240399bda2e4bef8f7dfb7de9a0f4f146d3
Vladimir Baryshnikov <vbaryshnikov@meetme.com>
2019-03-20 17:50:38 -0700

    Add repro steps for auto dispose lint error
    * What went wrong:
    Execution failed for task ':app:lintDebug'.
    > Lint found errors in the project; aborting build.
    
      Fix the issues identified by lint, or add the following to your build script to proceed with errors:
      ...
      android {
          lintOptions {
              abortOnError false
          }
      }
      ...
    
      Errors found:
    
      AutoDisposeLintError/app/src/main/java/com/example/autodisposelinterror/MainActivity.java:18: Error: Missing Disposable handling: Apply AutoDispose or cache the Disposable instance manually and enable lenient mode. [AutoDispose]
              disposables.add(Observable.just("").subscribe());
                              ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- app/src/main/java/com/example/autodisposelinterror/MainActivity.java
+++ app/src/main/java/com/example/autodisposelinterror/MainActivity.java
@@ -1,14 +1,26 @@
 package com.example.autodisposelinterror;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatActivity;
+
+import io.reactivex.Observable;
+import io.reactivex.disposables.CompositeDisposable;
+
 public class MainActivity extends AppCompatActivity {
+    private final CompositeDisposable disposables = new CompositeDisposable();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        disposables.add(Observable.just("").subscribe());
+    }
+
+    @Override
+    protected void onDestroy() {
+        disposables.clear();
+        super.onDestroy();
     }
 }
